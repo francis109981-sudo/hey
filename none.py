abc61be0539ea2ec1edd91e8cbc376e5777605df
francis <francis109981@gmail.com>
2025-11-15 19:06:30 +0600

    Update none.py

--- none.py
+++ none.py
@@ -5,24 +5,10 @@ from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
 
 
-tor_host = "127.0.0.1"
-tor_port = "9050"
-
-proxy_url = f"socks5h://{tor_host}:{tor_port}"
-proxies = {
-        "http": proxy_url,
-        "https": proxy_url,
-    }
-
-
-
 url1 = "https://facethepeople.net"
 url2 = "https://github.com/dulalahmed9701-lang/project-debian/releases/download/v1.1/debian-13.1.0-amd64-netinst.png"
 
 url = url1+"/_next/image?url="+url2+"&w=384&q=75"
-
-
-
 
 
 def req():
@@ -31,10 +17,8 @@ def req():
             response = requests.get(url)
             print(response.status_code)
         except Exception as e:
+            print(e)
             pass
-
-       
-
 
 
 
